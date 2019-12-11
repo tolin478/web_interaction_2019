1e0a02b30aa50e69997d168a61f0eb7f99150c33
Natasha Tolia <tolin478@newschool.edu>
2019-12-11 17:53:24 -0500

    Add files via upload

--- final/scripts.js
+++ final/scripts.js
@@ -48,36 +48,57 @@
 // }
 
 //for FOOD
-var foodArray = []
+var foodArray = ["Pumpkin beer","🍜","🍕","🍞","🍵","🐄","shallots","mango","Kbeebeeque","ithe cream","Zoupo"]
+
+//for ACTIVITIES
+var activitiesArray = ["Rafting and Mini Golfing","holiday inn express","golfland","🛒","💅"];
 
 //for RANDOM
-var descriptorArray = ["histamine-packed","goose skate park","Jeff bezos","malaysia","weeb","moana fan club","Pumpkin beer","Nyc","family","my",];
-var expletiveArray = ["fuckin","BITCH","hell"];
-var itemArray = ["Pumpkin beer","Rafting and Mini Golfing","mango","shallots","dinner","Kbeebeeque","holiday inn express","ithe cream","Zoupo","D E B T"];
-var paymentArray = ["scam","Allowance....","gluttony","hellhole","experience","entrance fee","extravaganza","dues","supper","wonderland"];
+var descriptorArray = ["histamine-packed","goose skate park","Jeff bezos","malaysia","weeb","moana fan club","Pumpkin beer","Nyc","family","my","glug glug","Dank","lots and lots of"];
+var expletiveArray = ["fuckin","BITCH","hell","bitch","binch"];
+var itemArray = ["Pumpkin beer","Rafting and Mini Golfing","mango","shallots","dinner","Kbeebeeque","holiday inn express","ithe cream","Zoupo","D E B T","golfland","pot","ubi","beep beep"];
+var paymentArray = ["scam","Allowance....","gluttony","hellhole","experience","entrance fee","extravaganza","dues","supper","wonderland","love and friendship"];
 var onelinerArray = ["crunch","yeet","Reparations", "Cloth","heheheheheheheheheheheehehehehe","Tiddy"];
 var emojis = ["🎄","🧂","🍭","🍜","🚘","🌃","🍕","🍞","🍵","💦","🐄","🛒","🥵","💅","🧀","🥖","🍋","🥂","🍧","🥗","🥧"]
 
-var mode = document.getElementById("chooseMode").value;
-
 function generate(){
+  var mode = document.getElementById("chooseMode").value;
+  console.log(mode);
+
+
   if(mode=="random"){
     randomGenerate();
+  }else if(mode=="food"){
+    foodGenerate();
+  }else if (mode=="acitivty"){
+    activityGenerate();
   }
 }
 
 function randomGenerate(){
+
+  var caption;
 
   var descriptor = descriptorArray[Math.floor(Math.random()*descriptorArray.length)];
   var expletive = expletiveArray[Math.floor(Math.random()*expletiveArray.length)];
   var item = itemArray[Math.floor(Math.random()*itemArray.length)];
   var payment = paymentArray[Math.floor(Math.random()*paymentArray.length)];
   var oneliner = onelinerArray[Math.floor(Math.random()*onelinerArray.length)];
+  var emoji = emojis[Math.floor(Math.random()*emojis.length)];
+
 
   var num1 = Math.floor(Math.random() * 50);
   var num2 = Math.floor(Math.random() * 100);
 
-  var caption = descriptor + " " + item + " " + payment;
+  var randomizer = Math.floor(Math.random() * 100);
+
+  if(randomizer<=50){
+    var caption = descriptor + " " + item + " " + payment;
+  }else if(randomizer>50 && randomizer<=75){
+    var caption = emoji;
+  }else if(randomizer>75 && randomizer<=100){
+    var caption = oneliner;
+  }
 
   if(num2>10){
     var price = "$" + num1 + "." + num2;
@@ -85,6 +106,41 @@ function randomGenerate(){
     var price = "$" + num1 + ".0" + num2;
   }
 
-  $(".caption").append("<p>" + caption);
-  $(".value").append("<p>" + price);
+  $(".caption").prepend("<p>" + caption);
+  $(".value").prepend("<p>" + price);
+}
+
+function foodGenerate(){
+
+  var descriptor = descriptorArray[Math.floor(Math.random()*descriptorArray.length)];
+  var expletive = expletiveArray[Math.floor(Math.random()*expletiveArray.length)];
+  var food = foodArray[Math.floor(Math.random()*foodArray.length)];
+  var payment = paymentArray[Math.floor(Math.random()*paymentArray.length)];
+  var oneliner = onelinerArray[Math.floor(Math.random()*onelinerArray.length)];
+
+  var num1 = Math.floor(Math.random() * 50);
+  var num2 = Math.floor(Math.random() * 100);
+
+  var randomizer = Math.floor(Math.random() * 100);
+
+  if(randomizer<=75){
+    var caption = descriptor + " " + food + " " + payment;
+  }else if(randomizer>75){
+    var caption = oneliner;
+  }
+
+  if(num2>10){
+    var price = "$" + num1 + "." + num2;
+  }else if(num2<10){
+    var price = "$" + num1 + ".0" + num2;
+  }
+
+  $(".caption").prepend("<p>" + caption);
+  $(".value").prepend("<p>" + price);
+
+
+}
+
+function activityGenerate(){
+
 }
