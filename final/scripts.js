ce55e6087b29b608bc2fd5ff6c9dd8cca12572c2
Natasha Tolia <tolin478@newschool.edu>
2019-12-12 18:11:49 -0500

    Add files via upload

--- final/scripts.js
+++ final/scripts.js
@@ -47,22 +47,309 @@
 //   }
 // }
 
+var listCaptions = [
+  "🌲🍭",
+  "Five bucks",
+  "🍜",
+  "Ubi🚗🌃",
+  "Yeet",
+  "Eesh",
+  "Allowance.....",
+  "East. I thought you said. Udon weast",
+  "Malaysia",
+  "Xo",
+  "Nyc gluttony",
+  "🍕",
+  "Pumpkin beer hell",
+  "Jeff bezos shallots scam",
+  "🍞🍵",
+  "💦",
+  "🍜",
+  "🐄",
+  "🛒",
+  "🥵",
+  "💅",
+  "No thai chicken rice allowed",
+  "oh and cleaning fee for cabin which was $60 flat rate :/",
+  "this venmo payment for gasoline and The Toll is one vote for MARIANNE",
+  "Lymes",
+  "If the hats missin......ive gone Rafting and Mini Golfing",
+  "T H M O R E S 🤗",
+  "Tarzan sex scene",
+  "🧀🍞🍋",
+  "🥂",
+  "🍧+🥗+🥧",
+  "drinks @ gingers/Italy etc",
+  "🌸",
+  "family dinner",
+  "bogo DO impress me much 🥵",
+  "Various shes",
+  "Kbeebeeque",
+  "she..... is.  ѕнe ιѕ. holiday inn express",
+  "golfland volcano hellhole",
+  "🥴,🍚",
+  "Zoupo 2.0",
+  "🍜",
+  "Zoupo",
+  "My D E B T",
+  "🆘🆘🆘🅱️",
+  "🍾",
+  "🍺",
+  "🍖",
+  "🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜🍜",
+  "🍜🍜🍜SLORP SLORP",
+  "🥤",
+  "🍰☕🍩",
+  "Cloth",
+  "🍜",
+  "🥐🍩",
+  "🍗",
+  "🚗💨",
+  "I calculated it wrong last time lmao",
+  "Beep beep binch",
+  "#declined",
+  "DUSHA",
+  "The ice cream",
+  "🇸🇪BRÖDER NÖRD🇸🇪",
+  "merci et á bientot 🇫🇷",
+  "Fuckin bun mee united scam t e a",
+  "goose skate park entrance fee",
+  "🅱️",
+  "HUEVOS~!",
+  "waiter~!",
+  "🍵🏄‍☘️🤑👒🐸",
+  "🍥🍥",
+  "this covers. the Catfish Belly",
+  "the IThcream 🍨",
+  "🍛",
+  "🥑",
+  "🌮",
+  "Weeb extravaganza",
+  "banh mi 🤸‍",
+  "🇻🇳",
+  "Le OOH eh le OOH",
+  "mango wonderland",
+  "🍴",
+  "🍙🍣",
+  "🍚",
+  "Gong xi fa cai🏮🎉",
+  "Taco$",
+  "I'm in love with the coco",
+  "lyft",
+  "🍝",
+  "💰",
+  "Love and friendship",
+  "🍅🌊🔥🥖",
+  "glug glug bitch",
+  "🍮",
+  "Dank pot",
+  "🎞️",
+  "😛😛😛😛😛😛😛😛😛",
+  "🌿",
+  "🍦",
+  "lots and lots of kale and quinoa🍃",
+  "🔙",
+  "⛩️🚄",
+  "bcuz Zelle snatched all my monetary possessions",
+  "😩thai😜house🕷️",
+  "☻",
+  "big fat juicy MEAT",
+  "🇯🇵hôtel",
+  "bronch",
+  "🍼",
+  "👹",
+  "🍤🍤🍤🍤🍤",
+  "heheheheheheheheheheheehehehehe",
+  "crunch",
+  "moana fan club dues",
+  "Reparations",
+  "naruto is our god",
+  "Recipe book from the creepy food anime",
+  "my undying affection :-*",
+  "Tiddy",
+  "I love you",
+  "the last supper",
+  "😘",
+  "BITCH"
+];
+
+var listValues = [
+  "$13.00",
+  "$5.00",
+  "$15.00",
+  "$15.00",
+  "$14.00",
+  "$25.00",
+  "$5.00",
+  "$15.00",
+  "$15.50",
+  "$15.50",
+  "$53.00",
+  "$2.10",
+  "$18.50",
+  "$5.50",
+  "$22.50",
+  "$15.00",
+  "$13.60",
+  "$15.00",
+  "$17.00",
+  "$14.00",
+  "$10.00",
+  "$12.00",
+  "$20.00",
+  "$38.66",
+  "$16.75",
+  "$54.00",
+  "$20.00",
+  "$15.00",
+  "$22.00",
+  "$15.00",
+  "$20.00",
+  "$28.00",
+  "$13.50",
+  "$28.00",
+  "$2.88",
+  "$22.00",
+  "$20.00",
+  "$64.32",
+  "$4.15",
+  "$18.50",
+  "$7.50",
+  "$24.00",
+  "$16.50",
+  "$27.00",
+  "$17.00",
+  "$75.00",
+  "$5.00",
+  "$5.00",
+  "$5.00",
+  "$12.00",
+  "$17.00",
+  "$17.00",
+  "$1.25",
+  "$21.00",
+  "$40.50",
+  "$18.40",
+  "$13.10",
+  "$5.00",
+  "$7.50",
+  "$16.50",
+  "$7.50",
+  "$34.50",
+  "$32.00",
+  "$18.00",
+  "$16.00",
+  "$9.95",
+  "$11.00",
+  "$21.00",
+  "$5.50",
+  "$3.00",
+  "$16.00",
+  "$19.00",
+  "$4.00",
+  "$3.00",
+  "$15.00",
+  "$17.50",
+  "$16.00",
+  "$5.00",
+  "$16.00",
+  "$17.50",
+  "$3.50",
+  "$11.75",
+  "$15.00",
+  "$18.00",
+  "$40.00",
+  "$10.00",
+  "$6.00",
+  "$21.00",
+  "$6.00",
+  "$30.00",
+  "$12.00",
+  "$8.50",
+  "$12.60",
+  "$7.00",
+  "$4.75",
+  "$20.25",
+  "$3.00",
+  "$3.00",
+  "$6.25",
+  "$3.00",
+  "$15.00",
+  "$10.25",
+  "$166.00",
+  "$7.00",
+  "$9.00",
+  "$50.00",
+  "$10.00",
+  "$60.00",
+  "$125.00",
+  "$200.00",
+  "$20.00",
+  "$18.00",
+  "$27.00",
+  "$345.00",
+  "$12.00",
+  "$14.00",
+  "$5.50",
+  "$11.00",
+  "$7.50",
+  "$12.00",
+  "$18.00",
+  "$10.00",
+  "$10.00",
+];
+
+console.log(listCaptions.length);
+console.log(listValues.length);
+
+for (i = 0; i < listCaptions.length; i++){
+  console.log(listCaptions[i]);
+  $(".listcaption").append("<p>" + listCaptions[i] + "</p>");
+}
+
+for (i = 0; i < listValues.length; i++){
+  console.log(listValues[i]);
+  $(".listprice").append("<p>" + listValues[i] + "</p>");
+}
+
+function sortCaptions(){
+
+  var sortChoice = document.getElementById("chooseSort").value;
+  console.log(sortChoice);
+
+  if(sortChoice=="chrono"){
+    $('.listcaption').empty();
+    for (i = 0; i < listCaptions.length; i++){
+      console.log(listCaptions[i]);
+      $(".listcaption").append("<p>" + listCaptions[i] + "</p>");
+    }
+  }else if(sortChoice=="alpha"){
+    $('.listcaption').empty();
+    listCaptions.sort();
+    for (i = 0; i < listCaptions.length; i++){
+      console.log(listCaptions[i]);
+      $(".listcaption").append("<p>" + listCaptions[i] + "</p>");
+    }
+  }
+
+}
+
 //for FOOD
 var foodArray = ["Pumpkin beer","shallots","mango","Kbeebeeque","ithe cream","Zoupo","banh mi","meat","taco bell","bronch"];
 var foodEmojiArray = ["🍜","🍕","🍞","🍵","🐄","🧀","🥖","🍋","🍧","🥗","🥧","🍺","🍖","🍰","🍩","🍗","🍥","🍛","🥑","🌮"];
 
 //for ACTIVITIES
 var activitiesArray = ["Rafting and Mini Golfing","holiday inn express","golfland","hôtel","parking","cabin"];
-var activityEmojiArray = ["🛒","💅","🚘","🌃","🅱️","🆘","🚗","🇰🇷","💨","🎬"];
+var activityEmojiArray = ["🛒","💅","🚘","🌃","🅱️","🆘","🚗","🇰🇷","💨","🎬","🔙"];
 
 //for RANDOM
-var descriptorArray = ["your","histamine-packed","goose skate park","Jeff bezos","malaysia","weeb","moana fan club","Pumpkin beer","Nyc","family","my","glug glug","Dank","lots and lots of","drinks @","bogo", "Various","Five","Past Due","for"];
+var descriptorArray = ["your","histamine-packed","goose skate park","Jeff bezos","malaysia","weeb","moana fan club","Pumpkin beer","Nyc","family","my","glug glug","Dank","lots and lots of","drinks @","bogo", "Various","Five","Past Due","for","Tarzan","she.......is."];
 // var expletiveArray = ["fuckin","BITCH","hell","bitch","binch"];
 var itemArray = ["Pumpkin beer","Rafting and Mini Golfing","mango","shallots","dinner","Kbeebeeque","holiday inn express","ithe cream","Zoupo","D E B T","golfland","pot","ubi","beep beep", "gasoline and The Toll", "banh mi","meat","hôtel","bronch","taco bell","parking","cabin"];
 var paymentArray = ["scam","Allowance....","gluttony","hellhole","experience","entrance fee","extravaganza","dues","supper","wonderland","love and friendship", "bucks", "shes","lyft","monies","cleaning fee"];
 var additionArrap = ["and one vote for MARIANNE", "DO impress me much","2.0"];
-var onelinerArray = ["crunch","yeet","Reparations", "Cloth","heheheheheheheheheheheehehehehe","Tiddy","ДУША","gong xi fa cai","I love you"];
-var emojis = ["🎄","🍭","🍜","🚘","🌃","🍕","🍞","🍵","💦","🐄","🛒","💅","🧀","🥖","🍋","🥂","🍧","🥗","🥧","🅱️","🆘","🍺","🍖","🍰","🍩","🍗","🚗","🇰🇷","💨","🍥","🎬","🍛","🥑","🌮"];
+var onelinerArray = ["crunch","yeet","Reparations", "Cloth","heheheheheheheheheheheehehehehe","Tiddy","gong xi fa cai","I love you","🍵🏄‍☘️🤑👒🐸"];
+var emojis = ["🎄","🍭","🍜","🚘","🌃","🍕","🍞","🍵","💦","🐄","🛒","💅","🧀","🥖","🍋","🥂","🍧","🥗","🥧","🅱️","🆘","🍺","🍖","🍰","🍩","🍗","🚗","🇰🇷","💨","🍥","🎬","🍛","🥑","🌮","🔙","🕷️"];
 
 function generate(){
   var mode = document.getElementById("chooseMode").value;
@@ -76,6 +363,7 @@ function generate(){
   }else if (mode=="activity"){
     activityGenerate();
   }
+
 }
 
 function randomGenerate(){
@@ -109,8 +397,8 @@ function randomGenerate(){
     var price = "$" + num1 + ".0" + num2;
   }
 
-  $(".caption").prepend("<p>" + caption);
-  $(".value").prepend("<p>" + price);
+  $(".caption").prepend("<p>" + caption + "</p>");
+  $(".value").prepend("<p>" + price + "</p>");
 }
 
 function foodGenerate(){
@@ -138,10 +426,8 @@ function foodGenerate(){
     var price = "$" + num1 + ".0" + num2;
   }
 
-  $(".caption").prepend("<p>" + caption);
-  $(".value").prepend("<p>" + price);
-
-
+  $(".caption").prepend("<p>" + caption + "</p>");
+  $(".value").prepend("<p>" + price + "</p>");
 }
 
 function activityGenerate(){
@@ -169,7 +455,20 @@ function activityGenerate(){
     var price = "$" + num1 + ".0" + num2;
   }
 
-  $(".caption").prepend("<p>" + caption);
-  $(".value").prepend("<p>" + price);
+  $(".caption").prepend("<p>" + caption + "</p>");
+  $(".value").prepend("<p>" + price + "</p>");
+}
 
+function displayList(){
+  $("#generateContainer").css("display","none");
+  $("#listDiv").css("display","block");
+
+  // $("body").css("background-color","aliceblue");
+}
+
+function displayGenerate(){
+  $("#generateContainer").css("display","block");
+  $("#listDiv").css("display","none");
+
+  // $("body").css("background-color","ghostwhite");
 }
